6e538e60b5697edc607c42293bc413b8f48ff1ff
Dasara <dasara.rexha03@gmail.com>
2023-05-25 21:32:51 -1200

    tailwind-css installed

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+  
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    )
 }
 
 export default App;
